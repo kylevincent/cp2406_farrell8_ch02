59c07ca473006b779bd591203ab1c50b51be38e5
jc431075 <kyle.vincent@my.jcu.edu.au>
2017-08-10 09:44:33 +1000

    Finished debugging exercises

--- DebuggingExercises/DebugTwo4.java
+++ DebuggingExercises/DebugTwo4.java
@@ -3,12 +3,12 @@ public class DebugTwo4
 {
    public static void main(String[] args)
    {
-      String costString
+      String costString;
       double cost;
-      final double TAX = 0.06;
-      costString = JOptionPane.showInputDialog("Enter price of item you are buying", "Purchases",
+      final double tax = 0.06;
+      costString = JOptionPane.showInputDialog(null, "Enter price of item you are buying", "Purchases",
          JOptionPane.INFORMATION_MESSAGE);
-      cost = Double.parseInt(costString);
+      cost = Double.parseDouble(costString);
       JOptionPane.showMessageDialog(null,"With " + tax * 100 +
          "% tax,  purchase  is $" + (cost - cost * tax));
    }
