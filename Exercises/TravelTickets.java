408305ab6b7afa21eb2455b8cdbc5329c20f88a7
kylevincent <kyle.vincent@my.jcu.edu.au>
2017-08-14 21:02:18 +1000

    Finished TravelTickets exercise

--- Exercises/TravelTickets.java
+++ Exercises/TravelTickets.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
+
 /**
  * Created by jc431075 on 11/08/17.
  */
-public class TravelTickets {
+public class TravelTickets
+{
+    public static void main(String[] args)
+    {
+        String GetTicketNumber = JOptionPane.showInputDialog(null, "Please enter ticket number");
+        String MakeActualTicketNumber = GetTicketNumber.substring(0, GetTicketNumber.length() - 1);
+        String MakeLastTicketNumber = GetTicketNumber.substring(GetTicketNumber.length() -1);
+        Long ActualTicketNumber = Long.parseLong(MakeActualTicketNumber);
+        Long LastTIcketNumber = Long.parseLong(MakeLastTicketNumber);
+        if (ActualTicketNumber % 7 == LastTIcketNumber )
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
 }
